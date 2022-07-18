50c9bda18af4dd20acb75143263503a8dfacd13c
Rashoumok <109136498+Rashoumok@users.noreply.github.com>
2022-07-18 06:17:17 -0400

    سورس.kr
    استفاده از این سورس در نرم افزار های ماشین حساب مجاز می باشد

---
+++ README.kt
@@ -0,0 +1,11 @@
+# -
+var a=readlian()
+var b=readlian()
+var c=readlian()
+var t=0
+if (b=="+"){a+c}
+else if (b=="-"){t=a-c}
+else if (b=="*"){t=a*c}
+else if (b=="/"){t=a/c}
+else if (b=="%"){t=a%c}
+println(t)
